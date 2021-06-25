bf877a29abf10e7a558577fde9b2e4b3e8b28da0
jaehwan_lim <jaehwan_lim@tmax.co.kr>
2021-06-25 11:18:55 +0900

    feat(ts): TS Programming 책 시작

---
+++ ts_programming/chap02/src/index.ts
@@ -0,0 +1,35 @@
+/* Chapter 01 */
+
+// 3 + []
+
+// let obj = {}
+// obj.foo
+
+// function a(b: number) {
+//     return b/2
+// }
+// a("z")
+
+/* Chapter 02 */
+
+// let a = 1 + 2
+// let b = a + 3
+// let c = {
+//     apple: a,
+//     banana: b
+// }
+// let d = c.apple * 4
+// let e = 'f'
+// let g = e + 1
+// let h = a + e
+// console.log(g, h)
+
+// let i = {
+//     a: 1,
+//     b: 'X'
+// }
+
+// let j = i.a + i.b
+// let k = i.a + 4
+// console.log('j:', j, ', k:', k)
+
